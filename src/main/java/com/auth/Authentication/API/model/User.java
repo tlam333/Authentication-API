9472cc2d409cc0d7e0399364cb6a88dc85a14748
tlam333 <161289007+tlam333@users.noreply.github.com>
2025-04-22 00:56:19 +1000

    adjusted security config configuarations + fixed postgresql error

--- src/main/java/com/auth/Authentication/API/model/User.java
+++ src/main/java/com/auth/Authentication/API/model/User.java
@@ -1,11 +1,9 @@
 package com.auth.Authentication.API.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id @GeneratedValue
     private Long id;
